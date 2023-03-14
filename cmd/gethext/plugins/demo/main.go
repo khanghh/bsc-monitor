97c21dfc87e8830441f7fde1a6efac914da3b8ea
Khang Hoang <hoanghongkhang@gmail.com>
2023-03-14 14:13:07 +0700

    [WIP] chain replayer

--- cmd/gethext/plugins/demo/main.go
+++ cmd/gethext/plugins/demo/main.go
@@ -1,14 +1,13 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"time"
 
+	"github.com/ethereum/go-ethereum/cmd/gethext/reexec"
 	"github.com/ethereum/go-ethereum/cmd/gethext/service/plugin"
 	"github.com/ethereum/go-ethereum/core/state"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/log"
+	"github.com/ethereum/go-ethereum/trie"
 )
 
 type DemoConfig struct {
@@ -21,65 +20,50 @@ type demoPlugin struct {
 }
 
 func (p *demoPlugin) execute(ctx *plugin.PluginCtx) {
-	runTimer := time.After(5 * time.Second)
+	bc := ctx.Eth.Chain()
+	db := state.NewDatabaseWithConfigAndCache(ctx.Eth.ChainDb(), &trie.Config{Cache: 16})
+	replayer := reexec.NewChainReplayer(db, bc, 100000)
+	block := bc.GetBlockByNumber(163454)
+	statedb, err := replayer.StateAtBlock(block)
+	if err != nil {
+		panic(err)
+	}
+	tr, err := statedb.Trie()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("root:", tr.Hash())
+
+	// statedb, err := replayer.ReplayTransaction(block, 1, nil)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// sender := common.HexToAddress("0x7ae2f5b9e386cd1b50a4550696d957cb4900f03a")
+	// balance := statedb.GetBalance(sender)
+	// if balance == nil {
+	// 	panic("no state")
+	// }
+	// fmt.Println("sender balance: ", balance.Uint64())
+}
+
+func (p *demoPlugin) runCountDown(ctx *plugin.PluginCtx) {
 	countDown := 5
+	runTimer := time.After(time.Duration(countDown) * time.Second)
 	for {
 		fmt.Printf("Run afeter %d seconds\n", countDown)
 		select {
 		case <-time.After(1 * time.Second):
 			countDown -= 1
 		case <-runTimer:
-			start := time.Now()
-			block, err := ctx.Eth.BlockByNumber(context.Background(), 7_099_870)
-			if err != nil {
-				log.Error("BlockByNumber", "error", err)
-			}
-			_, err = ctx.Eth.StateAtBlock(context.Background(), block, 100000, nil, false, true)
-			if err != nil {
-				log.Error("StateAndHeaderByNumber", "error", err)
-			}
-			elapsed := time.Since(start)
-			log.Info("StateAndHeaderByNumber", "elapse", elapsed)
+			p.execute(ctx)
 			return
 		}
 	}
 }
 
-func (p *demoPlugin) ProcessState(state *state.StateDB, block *types.Block, txIndex int) error {
-	fmt.Println("block", block.NumberU64())
-	fmt.Println("txIndex", txIndex)
-	fmt.Println("stateHash", state.StateIntermediateRoot())
-	return nil
-}
-
 func (p *demoPlugin) OnEnable(ctx *plugin.PluginCtx) error {
-	cfg := DemoConfig{}
-	if err := ctx.LoadConfig(&cfg); err != nil {
-		return err
-	}
-
-	// hasher := crypto.NewKeccakState()
-	// acc := common.HexToAddress("0x8894E0a0c962CB723c1976a4421c95949bE2D4E3")
-	// accHash := crypto.HashData(hasher, acc.Bytes())
-	// accData, err := snap.AccountRLP(accHash)
-	// if err != nil {
-	// 	ctx.Log.Error("Could not read account data", "error", err)
-	// }
-	// fmt.Println("accData size:", len(accData))
-	// api := ethapi.NewPublicBlockChainAPI(ctx.Eth)
-	// fmt.Println(cfg)
-
-	// ctx.Log.Info("Demo plugin enabled!")
-	// task, err := ctx.ReExec.RunTask(reexec.ReExecOptions{
-	// 	StartBlock: 5000000,
-	// 	EndBlock:   5100000,
-	// 	Processors: []reexec.Processor{p},
-	// })
-	// if err != nil {
-	// 	ctx.Log.Error("failed to run task", "error", err)
-	// 	return nil
-	// }
-	// task.Wait()
+	ctx.Log.Info("Demo plugin enabled!")
+	go p.runCountDown(ctx)
 	return nil
 }
 
