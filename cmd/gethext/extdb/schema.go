9e03c645535dbc83e9c08d9fd73c8a113394f424
Khang Hoang <hoanghongkhang@gmail.com>
2023-03-21 13:25:17 +0700

    update extdb schema

--- cmd/gethext/extdb/schema.go
+++ cmd/gethext/extdb/schema.go
@@ -15,17 +15,18 @@ import (
 )
 
 var (
-	LastIndexStateKey = []byte("LastIndexState") // LastIndexState tracks the hash of the last indexed state.
-	LastIndexBlockKey = []byte("LastIndexBlock") // LastIndexBlock tracks the hash of the last indexed block.
+	LastIndexStateKey = []byte("LastIndexState") // LastIndexState tracks the hash of the last indexed statedb
+	LastIndexBlockKey = []byte("LastIndexBlock") // LastIndexBlock tracks the hash of the last indexed block
 	TotalAccountsKey  = []byte("TotalAccounts")
 	TotalContractsKey = []byte("TotalContracts")
 
 	AccountInfoPrefix       = []byte("a") // AccountInfoPrefix + address -> account info
-	ContractInfoPrefix      = []byte("c") // ContractInfoPrefix + address -> contract info
-	AccountStatePrefix      = []byte("s") // AccountStatePrefix + hash(StateAccount) -> account ext state
-	AccountSentTxPrefix     = []byte("t") // AccountSentTxPrefix + address + nonce -> transaction hash
-	AccountInternalTxPrefix = []byte("i") // AccountInternalTxPrefix + address + index -> transaction hash
+	ContractABIPrefix       = []byte("c") // ContractInfoPrefix + address -> contract info
+	AccountIndexStatePrefix = []byte("s") // AccountIndexStatePrefix + hash(StateAccount) -> account index state
+	AccountSentTxPrefix     = []byte("t") // AccountSentTxPrefix + address + block num + index -> transaction hash
+	AccountInternalTxPrefix = []byte("i") // AccountInternalTxPrefix + address + block num + index -> transaction hash
 	AccountTokenTxPrefix    = []byte("x") // AccountTokenTxPrefix + address + index -> transaction hash
+	TokenHolderAddrPrefix   = []byte("h") // TokenHolderPrefix + token address + index -> account address
 )
 
 var (
@@ -39,29 +40,45 @@ func AccountInfoKey(addr common.Address) []byte {
 
 func ContractInfoKey(addr common.Address) []byte {
 	addrHash := crypto.Keccak256Hash(addr.Bytes())
-	return append(ContractInfoPrefix, addrHash.Bytes()...)
+	return append(ContractABIPrefix, addrHash.Bytes()...)
 }
 
-func AccountExtStateKey(root common.Hash) []byte {
-	return append(AccountStatePrefix, root.Bytes()...)
+func AccountIndexStateKey(hash common.Hash) []byte {
+	return append(AccountIndexStatePrefix, hash.Bytes()...)
 }
 
-func tableElementKey(prefix []byte, addr common.Address, index uint64) []byte {
+func indexItemKey(prefix []byte, addr common.Address, refNum uint64) []byte {
 	buf := new(bytes.Buffer)
 	binary.Write(buf, binary.BigEndian, prefix)
 	binary.Write(buf, binary.BigEndian, addr.Bytes())
-	binary.Write(buf, binary.BigEndian, index)
+	binary.Write(buf, binary.BigEndian, refNum)
 	return buf.Bytes()
 }
 
-func AccountSentTxKey(addr common.Address, nonce uint64) []byte {
-	return tableElementKey(AccountSentTxPrefix, addr, nonce)
+func IndexItemRef(blockNumber uint64, index uint64) []byte {
+	buf := make([]byte, 8)
+	binary.BigEndian.PutUint32(buf[:4], uint32(blockNumber))
+	binary.BigEndian.PutUint32(buf[4:], uint32(index))
+	return buf
 }
 
-func AccountInternalTxKey(addr common.Address, nonce uint64) []byte {
-	return tableElementKey(AccountInternalTxPrefix, addr, nonce)
+func IndexItemRefNum(blockNumber uint64, index uint64) uint64 {
+	buf := IndexItemRef(blockNumber, index)
+	return binary.BigEndian.Uint64(buf)
 }
 
-func AccountTokenTxKey(addr common.Address, nonce uint64) []byte {
-	return tableElementKey(AccountTokenTxPrefix, addr, nonce)
+func AccountSentTxKey(addr common.Address, refNum uint64) []byte {
+	return indexItemKey(AccountSentTxPrefix, addr, refNum)
+}
+
+func AccountInternalTxKey(addr common.Address, refNum uint64) []byte {
+	return indexItemKey(AccountInternalTxPrefix, addr, refNum)
+}
+
+func AccountTokenTxKey(addr common.Address, refNum uint64) []byte {
+	return indexItemKey(AccountTokenTxPrefix, addr, refNum)
+}
+
+func TokenHolderAddrKey(tknAddr common.Address, refNum uint64) []byte {
+	return indexItemKey(TokenHolderAddrPrefix, tknAddr, refNum)
 }
---
+++ cmd/gethext/extdb/trieext.go
@@ -0,0 +1,132 @@
+package extdb
+
+import (
+	"sync"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/state"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethdb"
+	"github.com/ethereum/go-ethereum/rlp"
+	"github.com/ethereum/go-ethereum/trie"
+)
+
+type RLPUnmarshaler interface {
+	UnmarshalRLP([]byte) error
+}
+
+// TrieExt store extra data for key-value pairs in trie,
+type TrieExt struct {
+	diskdb   ethdb.Database
+	trie     state.Trie
+	prefix   []byte
+	keyCache map[common.Hash][]byte // map hash(key) => ext key
+	changes  map[common.Hash][]byte // map hash(key) => ext val
+	mtx      sync.Mutex
+}
+
+func (s *TrieExt) GetExtKey(key []byte) ([]byte, error) {
+	hashKey := crypto.Keccak256Hash(key)
+	if extKey, exist := s.keyCache[hashKey]; exist {
+		return extKey, nil
+	}
+	val, err := s.trie.TryGet(key)
+	if err != nil {
+		return nil, err
+	}
+	s.keyCache[hashKey] = crypto.Keccak256Hash(key, val).Bytes()
+	return s.keyCache[hashKey], nil
+}
+
+// Trie returns underlying trie of the ExtTrie
+func (s *TrieExt) Trie() state.Trie {
+	return s.trie
+}
+
+// TryGet return custom data of key, the key is the same to original trie
+func (s *TrieExt) TryGet(key []byte) ([]byte, error) {
+	s.mtx.Lock()
+	defer s.mtx.Unlock()
+	if cached, exist := s.changes[crypto.Keccak256Hash(key)]; exist {
+		return cached, nil
+	}
+	extKey, err := s.GetExtKey(key)
+	if err != nil {
+		return nil, err
+	}
+	return s.diskdb.Get(append(s.prefix, extKey...))
+}
+
+// Get is the same as TryGet but ignore the error
+func (s *TrieExt) Get(key []byte) []byte {
+	data, _ := s.TryGet(key)
+	return data
+}
+
+func (s *TrieExt) Hash() common.Hash {
+	return s.trie.Hash()
+}
+
+func (s *TrieExt) RLPDecode(key []byte, val interface{}) error {
+	data, err := s.TryGet(key)
+	if err != nil {
+		return err
+	}
+	if decoder, ok := val.(RLPUnmarshaler); ok {
+		return decoder.UnmarshalRLP(data)
+	}
+	return rlp.DecodeBytes(data, &val)
+}
+
+func (s *TrieExt) TryUpdate(key, value []byte) error {
+	s.mtx.Lock()
+	defer s.mtx.Unlock()
+	if len(value) > 0 {
+		if _, err := s.GetExtKey(key); err != nil {
+			return err
+		}
+		s.changes[crypto.Keccak256Hash(key)] = value
+	}
+	return nil
+}
+
+func (s *TrieExt) TryDelete(key []byte) error {
+	return s.TryUpdate(key, nil)
+}
+
+// Commit write all changes to db
+func (s *TrieExt) Commit(writer ethdb.KeyValueWriter) error {
+	s.mtx.Lock()
+	defer s.mtx.Unlock()
+	if writer == nil {
+		writer = s.diskdb
+	}
+	iter := trie.NewIterator(s.trie.NodeIterator([]byte{}))
+	var err error
+	for iter.Next() {
+		val, exist := s.changes[crypto.Keccak256Hash(iter.Key)]
+		if !exist {
+			continue
+		}
+		dbKey := append(s.prefix, crypto.Keccak256Hash(iter.Value).Bytes()...)
+		if len(val) > 0 {
+			err = writer.Put(dbKey[:], val)
+		} else {
+			err = writer.Delete(dbKey[:])
+		}
+		if err != nil {
+			return err
+		}
+	}
+	return iter.Err
+}
+
+func NewTrieExt(db ethdb.Database, tr state.Trie, prefix []byte) *TrieExt {
+	return &TrieExt{
+		diskdb:   db,
+		trie:     tr,
+		prefix:   prefix,
+		keyCache: make(map[common.Hash][]byte),
+		changes:  make(map[common.Hash][]byte),
+	}
+}
