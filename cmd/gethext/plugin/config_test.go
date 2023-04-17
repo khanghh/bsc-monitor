68bef0b7dccdec33d0773cb660fdbea7aa380ab4
Khang Hoang <hoanghongkhang@gmail.com>
2023-04-17 16:19:51 +0700

    update plugins config

---
+++ cmd/gethext/plugin/config_test.go
@@ -0,0 +1,21 @@
+package plugin
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestConfigStore(t *testing.T) {
+	configStore := NewConfigStore("Plugins", "config.test.toml")
+	type DemoConfig struct {
+		Field1 bool
+		Field2 string
+	}
+	cfg := DemoConfig{}
+	if err := configStore.loadConfig("Demo", &cfg); err != nil {
+		panic(err)
+	}
+	assert.Equal(t, true, cfg.Field1)
+	assert.Equal(t, "test", cfg.Field2)
+}
