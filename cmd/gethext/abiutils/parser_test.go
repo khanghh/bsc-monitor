96bc922f4e405dfe6ec2320cea2241dc05bfea76
Khang Hoang <hoanghongkhang@gmail.com>
2023-05-04 15:29:48 +0700

    fix: get interface list from 4-bytes sigs

--- cmd/gethext/abiutils/parser_test.go
+++ cmd/gethext/abiutils/parser_test.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"os"
 	"testing"
+
+	"github.com/ethereum/go-ethereum/core/rawdb"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestParseMethodIds(t *testing.T) {
@@ -21,4 +24,48 @@ func TestParseMethodIds(t *testing.T) {
 	for _, id := range methodIds {
 		fmt.Println(id)
 	}
+}
+
+func TestABIParserGetInterfaces(t *testing.T) {
+	unknowns := []string{
+		"aabb1337", // test unknown methods
+		"abcdefaa", // test unknown methods
+	}
+	testERC20Sigs := []string{
+		"dd62ed3e", // allowance(address,address)
+		"095ea7b3", // approve(address,uint256)
+		"70a08231", // balanceOf(address)
+		"18160ddd", // totalSupply()
+		"a9059cbb", // transfer(address,uint256)
+		"23b872dd", // transferFrom(address,address,uint256)
+		"aabb1337", // test unknown methods
+		"abcdefaa", // test unknown methods
+	}
+	testERC721Sigs := []string{
+		"095ea7b3", // approve(address,uint256)
+		"70a08231", // balanceOf(address)
+		"081812fc", // getApproved(uint256)
+		"e985e9c5", // isApprovedForAll(address,address)
+		"6352211e", // ownerOf(uint256)
+		"42842e0e", // safeTransferFrom(address,address,uint256)
+		"b88d4fde", // safeTransferFrom(address,address,uint256,bytes)
+		"a22cb465", // setApprovalForAll(address,bool)
+		"23b872dd", // transferFrom(address,address,uint256)
+		"aabb1337", // test unknown methods
+		"abcdefaa", // test unknown methods
+	}
+	getNameList := func(ifs []Interface) []string {
+		ret := make([]string, 0, len(ifs))
+		for _, item := range ifs {
+			ret = append(ret, item.Name)
+		}
+		return ret
+	}
+	parser := NewParser(rawdb.NewMemoryDatabase())
+	ifs, remaining := parser.GetInterfaces(testERC20Sigs)
+	assert.Contains(t, getNameList(ifs), "IERC20")
+	assert.Equal(t, remaining, unknowns)
+	ifs, remaining = parser.GetInterfaces(testERC721Sigs)
+	assert.Contains(t, getNameList(ifs), "IERC721")
+	assert.Equal(t, remaining, unknowns)
 }
