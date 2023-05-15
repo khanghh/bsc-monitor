7d372d8e2047fb2f0b9da5951b64b092f5c51c8d
Khang Hoang <hoanghongkhang@gmail.com>
2023-05-15 13:07:26 +0700

    chore(whalemonitor): update thresholds

--- cmd/gethext/plugins/whalemonitor/cmd/main.go
+++ cmd/gethext/plugins/whalemonitor/cmd/main.go
@@ -26,16 +26,16 @@ const (
 
 var (
 	logger                    = plugin.NewLogger(pluginNamespace)
-	bigETHTransferThreshold   = new(big.Int).Mul(big.NewInt(100), big.NewInt(params.Ether))
+	bigETHTransferThreshold   = new(big.Int).Mul(big.NewInt(500), big.NewInt(params.Ether))
 	bigERC20TransferThreshold = map[common.Address]float64{
-		common.HexToAddress("0xA3183498b579bd228aa2B62101C40CC1da978F24"): 50000, // test token
-		common.HexToAddress("0x55d398326f99059fF775485246999027B3197955"): 50000, // USDT
-		common.HexToAddress("0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56"): 50000, // BUSD
-		common.HexToAddress("0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c"): 100,   // WBNB
-		common.HexToAddress("0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c"): 2,     // BTCB
-		common.HexToAddress("0x2170Ed0880ac9A755fd29B2688956BD959F933F8"): 50,    // WETH
-		common.HexToAddress("0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d"): 50000, // USDC
-		common.HexToAddress("0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82"): 10000, // CAKE
+		common.HexToAddress("0xA3183498b579bd228aa2B62101C40CC1da978F24"): 50000,  // test token
+		common.HexToAddress("0x55d398326f99059fF775485246999027B3197955"): 200000, // USDT
+		common.HexToAddress("0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56"): 200000, // BUSD
+		common.HexToAddress("0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c"): 500,    // WBNB
+		common.HexToAddress("0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c"): 7,      // BTCB
+		common.HexToAddress("0x2170Ed0880ac9A755fd29B2688956BD959F933F8"): 100,    // WETH
+		common.HexToAddress("0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d"): 200000, // USDC
+		common.HexToAddress("0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82"): 10000,  // CAKE
 	}
 )
 
@@ -76,7 +76,7 @@ func (t *txProcessor) processCallFrame(frame *reexec.CallFrame) {
 	if frame.Value != nil {
 		if frame.Value.Cmp(bigETHTransferThreshold) > 0 {
 			txHash := t.txResult.Transaction.Hash()
-			logger.Info("Big ETH transfer", "from", frame.From, "to", frame.To, "value", AmountString(frame.Value, 18), "tx", txHash.Hex())
+			logger.Info("Big BNB transfer", "from", frame.From, "to", frame.To, "value", AmountString(frame.Value, 18), "tx", txHash.Hex())
 			t.whaleFeed.Send(whalemonitor.WhaleEvent{
 				TxHash: txHash,
 				From:   frame.From,
