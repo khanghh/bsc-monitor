d561215d0ee399cb933b1ddad977beafb7b4f0fd
Khang Hoang <hoanghongkhang@gmail.com>
2023-06-20 16:15:34 +0700

    feat(explorer): service stack get database by namespace

---
+++ cmd/explorer/service/errors.go
@@ -0,0 +1,29 @@
+package service
+
+import (
+	"errors"
+	"fmt"
+	"reflect"
+)
+
+var (
+	ErrDatadirUsed     = errors.New("datadir already used by another process")
+	ErrServiceStopping = errors.New("service is stopping")
+	ErrServiceRunning  = errors.New("service already running")
+	ErrServiceUnknown  = errors.New("unknown service")
+	ErrNoDatabase      = errors.New("no database")
+
+	datadirInUseErrnos = map[uint]bool{11: true, 32: true, 35: true}
+)
+
+// StopError is returned if a Node fails to stop either any of its registered
+// services or itself.
+type StopError struct {
+	Server   error
+	Services map[reflect.Type]error
+}
+
+// Error generates a textual representation of the stop error.
+func (e *StopError) Error() string {
+	return fmt.Sprintf("server: %v, services: %v", e.Server, e.Services)
+}
