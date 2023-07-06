cdb41d44fc324e47efe3ae7f66243907b88c9cd4
Khang Hoang <hoanghongkhang@gmail.com>
2023-07-06 22:33:02 +0700

    docs: remove unnecessary header from the code

---
+++ cmd/explorer/indexer/indexer.go
@@ -0,0 +1,15 @@
+package indexer
+
+import (
+	"github.com/ethereum/go-ethereum/core/vm"
+	"github.com/ethereum/go-ethereum/ethdb"
+)
+
+type Indexer struct {
+}
+
+func (idx *Indexer) EVMTracer() vm.EVMLogger
+
+func New(config *Config, indexDb ethdb.Database) (*Indexer, error) {
+	return nil, nil
+}
