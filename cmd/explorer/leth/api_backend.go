bbab4ad87dd9580f2a8a58f74efe6d73f7601bc4
Khang Hoang <hoanghongkhang@gmail.com>
2023-07-07 10:51:14 +0700

    refactor(leth): remove unused code and improve naming conventions

--- cmd/explorer/leth/api_backend.go
+++ cmd/explorer/leth/api_backend.go
@@ -21,63 +21,63 @@ import (
 	"github.com/ethereum/go-ethereum/rpc"
 )
 
-// EthAPIBackend implements internal/ethapi.Backend
-type EthAPIBackend struct {
+// LEthAPIBackend implements internal/ethapi.Backend
+type LEthAPIBackend struct {
 	leth *LightEthereum
 }
 
 // General Ethereum API
-func (b *EthAPIBackend) SyncProgress() ethereum.SyncProgress {
+func (b *LEthAPIBackend) SyncProgress() ethereum.SyncProgress {
 	panic("not implement")
 }
 
-func (b *EthAPIBackend) SuggestGasTipCap(ctx context.Context) (*big.Int, error) {
+func (b *LEthAPIBackend) SuggestGasTipCap(ctx context.Context) (*big.Int, error) {
 	panic("not implement")
 }
 
-func (b *EthAPIBackend) FeeHistory(ctx context.Context, blockCount int, lastBlock rpc.BlockNumber, rewardPercentiles []float64) (*big.Int, [][]*big.Int, []*big.Int, []float64, error) {
+func (b *LEthAPIBackend) FeeHistory(ctx context.Context, blockCount int, lastBlock rpc.BlockNumber, rewardPercentiles []float64) (*big.Int, [][]*big.Int, []*big.Int, []float64, error) {
 	panic("not implement")
 }
 
-func (b *EthAPIBackend) Chain() *core.BlockChain {
+func (b *LEthAPIBackend) Chain() *core.BlockChain {
 	return b.leth.blockchain
 }
 
-func (b *EthAPIBackend) ChainDb() ethdb.Database {
+func (b *LEthAPIBackend) ChainDb() ethdb.Database {
 	panic("not implement")
 }
 
-func (b *EthAPIBackend) AccountManager() *accounts.Manager {
+func (b *LEthAPIBackend) AccountManager() *accounts.Manager {
 	panic("not implement")
 }
 
-func (b *EthAPIBackend) ExtRPCEnabled() bool {
+func (b *LEthAPIBackend) ExtRPCEnabled() bool {
 	panic("not implement")
 }
 
-func (b *EthAPIBackend) RPCGasCap() uint64 {
+func (b *LEthAPIBackend) RPCGasCap() uint64 {
 	return b.leth.config.RPCGasCap
 }
 
-func (b *EthAPIBackend) RPCEVMTimeout() time.Duration {
+func (b *LEthAPIBackend) RPCEVMTimeout() time.Duration {
 	return b.leth.config.RPCEVMTimeout
 }
 
-func (b *EthAPIBackend) RPCTxFeeCap() float64 {
+func (b *LEthAPIBackend) RPCTxFeeCap() float64 {
 	panic("not implement")
 }
 
-func (b *EthAPIBackend) UnprotectedAllowed() bool {
+func (b *LEthAPIBackend) UnprotectedAllowed() bool {
 	panic("not implement")
 }
 
 // Blockchain API
-func (b *EthAPIBackend) SetHead(number uint64) {
+func (b *LEthAPIBackend) SetHead(number uint64) {
 	// b.eth.handler.downloader.Cancel()
 	b.leth.blockchain.SetHead(number)
 }
 
-func (b *EthAPIBackend) HeaderByNumber(ctx context.Context, number rpc.BlockNumber) (*types.Header, error) {
+func (b *LEthAPIBackend) HeaderByNumber(ctx context.Context, number rpc.BlockNumber) (*types.Header, error) {
 	// Pending block is only known by the miner
 	if number == rpc.PendingBlockNumber {
 		return nil, nil
@@ -89,11 +89,11 @@ func (b *EthAPIBackend) HeaderByNumber(ctx context.Context, number rpc.BlockNumb
 	return b.leth.blockchain.GetHeaderByNumber(uint64(number)), nil
 }
 
-func (b *EthAPIBackend) HeaderByHash(ctx context.Context, hash common.Hash) (*types.Header, error) {
+func (b *LEthAPIBackend) HeaderByHash(ctx context.Context, hash common.Hash) (*types.Header, error) {
 	return b.leth.blockchain.GetHeaderByHash(hash), nil
 }
 
-func (b *EthAPIBackend) HeaderByNumberOrHash(ctx context.Context, blockNrOrHash rpc.BlockNumberOrHash) (*types.Header, error) {
+func (b *LEthAPIBackend) HeaderByNumberOrHash(ctx context.Context, blockNrOrHash rpc.BlockNumberOrHash) (*types.Header, error) {
 	if blockNr, ok := blockNrOrHash.Number(); ok {
 		return b.HeaderByNumber(ctx, blockNr)
 	}
@@ -110,15 +110,15 @@ func (b *EthAPIBackend) HeaderByNumberOrHash(ctx context.Context, blockNrOrHash 
 	return nil, errors.New("invalid arguments; neither block nor hash specified")
 }
 
-func (b *EthAPIBackend) CurrentHeader() *types.Header {
+func (b *LEthAPIBackend) CurrentHeader() *types.Header {
 	return b.leth.blockchain.CurrentHeader()
 }
 
-func (b *EthAPIBackend) CurrentBlock() *types.Block {
+func (b *LEthAPIBackend) CurrentBlock() *types.Block {
 	return b.leth.blockchain.CurrentBlock()
 }
 
-func (b *EthAPIBackend) BlockByNumber(ctx context.Context, number rpc.BlockNumber) (*types.Block, error) {
+func (b *LEthAPIBackend) BlockByNumber(ctx context.Context, number rpc.BlockNumber) (*types.Block, error) {
 	// Pending block is only known by the miner
 	if number == rpc.PendingBlockNumber {
 		return nil, nil
@@ -130,11 +130,11 @@ func (b *EthAPIBackend) BlockByNumber(ctx context.Context, number rpc.BlockNumbe
 	return b.leth.blockchain.GetBlockByNumber(uint64(number)), nil
 }
 
-func (b *EthAPIBackend) BlockByHash(ctx context.Context, hash common.Hash) (*types.Block, error) {
+func (b *LEthAPIBackend) BlockByHash(ctx context.Context, hash common.Hash) (*types.Block, error) {
 	return b.leth.blockchain.GetBlockByHash(hash), nil
 }
 
-func (b *EthAPIBackend) BlockByNumberOrHash(ctx context.Context, blockNrOrHash rpc.BlockNumberOrHash) (*types.Block, error) {
+func (b *LEthAPIBackend) BlockByNumberOrHash(ctx context.Context, blockNrOrHash rpc.BlockNumberOrHash) (*types.Block, error) {
 	if blockNr, ok := blockNrOrHash.Number(); ok {
 		return b.BlockByNumber(ctx, blockNr)
 	}
@@ -155,7 +155,7 @@ func (b *EthAPIBackend) BlockByNumberOrHash(ctx context.Context, blockNrOrHash r
 	return nil, errors.New("invalid arguments; neither block nor hash specified")
 }
 
-func (b *EthAPIBackend) StateAndHeaderByNumber(ctx context.Context, number rpc.BlockNumber) (*state.StateDB, *types.Header, error) {
+func (b *LEthAPIBackend) StateAndHeaderByNumber(ctx context.Context, number rpc.BlockNumber) (*state.StateDB, *types.Header, error) {
 	// Pending state is only known by the miner
 	if number == rpc.PendingBlockNumber {
 		return nil, nil, nil
@@ -172,7 +172,7 @@ func (b *EthAPIBackend) StateAndHeaderByNumber(ctx context.Context, number rpc.B
 	return stateDb, header, err
 }
 
-func (b *EthAPIBackend) StateAndHeaderByNumberOrHash(ctx context.Context, blockNrOrHash rpc.BlockNumberOrHash) (*state.StateDB, *types.Header, error) {
+func (b *LEthAPIBackend) StateAndHeaderByNumberOrHash(ctx context.Context, blockNrOrHash rpc.BlockNumberOrHash) (*state.StateDB, *types.Header, error) {
 	if blockNr, ok := blockNrOrHash.Number(); ok {
 		return b.StateAndHeaderByNumber(ctx, blockNr)
 	}
@@ -193,22 +193,22 @@ func (b *EthAPIBackend) StateAndHeaderByNumberOrHash(ctx context.Context, blockN
 	return nil, nil, errors.New("invalid arguments; neither block nor hash specified")
 }
 
-func (b *EthAPIBackend) PendingBlockAndReceipts() (*types.Block, types.Receipts) {
+func (b *LEthAPIBackend) PendingBlockAndReceipts() (*types.Block, types.Receipts) {
 	panic("not implement")
 }
 
-func (b *EthAPIBackend) GetReceipts(ctx context.Context, hash common.Hash) (types.Receipts, error) {
+func (b *LEthAPIBackend) GetReceipts(ctx context.Context, hash common.Hash) (types.Receipts, error) {
 	panic("not implement")
 }
 
-func (b *EthAPIBackend) GetTd(ctx context.Context, hash common.Hash) *big.Int {
+func (b *LEthAPIBackend) GetTd(ctx context.Context, hash common.Hash) *big.Int {
 	if header := b.leth.blockchain.GetHeaderByHash(hash); header != nil {
 		return b.leth.blockchain.GetTd(hash, header.Number.Uint64())
 	}
 	return nil
 }
 
-func (b *EthAPIBackend) GetEVM(ctx context.Context, msg core.Message, state *state.StateDB, header *types.Header, vmConfig *vm.Config) (*vm.EVM, func() error, error) {
+func (b *LEthAPIBackend) GetEVM(ctx context.Context, msg core.Message, state *state.StateDB, header *types.Header, vmConfig *vm.Config) (*vm.EVM, func() error, error) {
 	vmError := func() error { return nil }
 	if vmConfig == nil {
 		vmConfig = b.leth.blockchain.GetVMConfig()
@@ -218,100 +218,100 @@ func (b *EthAPIBackend) GetEVM(ctx context.Context, msg core.Message, state *sta
 	return vm.NewEVM(context, txContext, state, b.leth.blockchain.Config(), *vmConfig), vmError, nil
 }
 
-func (b *EthAPIBackend) SubscribeChainEvent(ch chan<- core.ChainEvent) event.Subscription {
+func (b *LEthAPIBackend) SubscribeChainEvent(ch chan<- core.ChainEvent) event.Subscription {
 	return b.leth.BlockChain().SubscribeChainEvent(ch)
 }
 
-func (b *EthAPIBackend) SubscribeChainHeadEvent(ch chan<- core.ChainHeadEvent) event.Subscription {
+func (b *LEthAPIBackend) SubscribeChainHeadEvent(ch chan<- core.ChainHeadEvent) event.Subscription {
 	return b.leth.BlockChain().SubscribeChainHeadEvent(ch)
 }
 
-func (b *EthAPIBackend) SubscribeChainSideEvent(ch chan<- core.ChainSideEvent) event.Subscription {
+func (b *LEthAPIBackend) SubscribeChainSideEvent(ch chan<- core.ChainSideEvent) event.Subscription {
 	return b.leth.BlockChain().SubscribeChainSideEvent(ch)
 }
 
 // Transaction pool API
-func (b *EthAPIBackend) SendTx(ctx context.Context, signedTx *types.Transaction) error {
+func (b *LEthAPIBackend) SendTx(ctx context.Context, signedTx *types.Transaction) error {
 	panic("not implement")
 }
 
-func (b *EthAPIBackend) GetTransaction(ctx context.Context, txHash common.Hash) (*types.Transaction, common.Hash, uint64, uint64, error) {
+func (b *LEthAPIBackend) GetTransaction(ctx context.Context, txHash common.Hash) (*types.Transaction, common.Hash, uint64, uint64, error) {
 	panic("not implement")
 }
 
-func (b *EthAPIBackend) GetPoolTransactions() (types.Transactions, error) {
+func (b *LEthAPIBackend) GetPoolTransactions() (types.Transactions, error) {
 	panic("not implement")
 }
 
-func (b *EthAPIBackend) GetPoolTransaction(txHash common.Hash) *types.Transaction {
+func (b *LEthAPIBackend) GetPoolTransaction(txHash common.Hash) *types.Transaction {
 	panic("not implement")
 }
 
-func (b *EthAPIBackend) GetPoolNonce(ctx context.Context, addr common.Address) (uint64, error) {
+func (b *LEthAPIBackend) GetPoolNonce(ctx context.Context, addr common.Address) (uint64, error) {
 	panic("not implement")
 }
 
-func (b *EthAPIBackend) Stats() (pending int, queued int) {
+func (b *LEthAPIBackend) Stats() (pending int, queued int) {
 	panic("not implement")
 }
 
-func (b *EthAPIBackend) TxPoolContent() (map[common.Address]types.Transactions, map[common.Address]types.Transactions) {
+func (b *LEthAPIBackend) TxPoolContent() (map[common.Address]types.Transactions, map[common.Address]types.Transactions) {
 	panic("not implement")
 }
 
-func (b *EthAPIBackend) TxPoolContentFrom(addr common.Address) (types.Transactions, types.Transactions) {
+func (b *LEthAPIBackend) TxPoolContentFrom(addr common.Address) (types.Transactions, types.Transactions) {
 	panic("not implement")
 }
 
-func (b *EthAPIBackend) SubscribeNewTxsEvent(ch chan<- core.NewTxsEvent) event.Subscription {
+func (b *LEthAPIBackend) SubscribeNewTxsEvent(ch chan<- core.NewTxsEvent) event.Subscription {
 	return b.leth.TxPool().SubscribeNewTxsEvent(ch)
 }
 
 // Filter API
-func (b *EthAPIBackend) BloomStatus() (uint64, uint64) {
+func (b *LEthAPIBackend) BloomStatus() (uint64, uint64) {
 	panic("not implement")
 }
 
-func (b *EthAPIBackend) GetLogs(ctx context.Context, blockHash common.Hash) ([][]*types.Log, error) {
+func (b *LEthAPIBackend) GetLogs(ctx context.Context, blockHash common.Hash) ([][]*types.Log, error) {
 	panic("not implement")
 }
 
-func (b *EthAPIBackend) ServiceFilter(ctx context.Context, session *bloombits.MatcherSession) {
+func (b *LEthAPIBackend) ServiceFilter(ctx context.Context, session *bloombits.MatcherSession) {
 	panic("not implement")
 }
 
-func (b *EthAPIBackend) SubscribeLogsEvent(ch chan<- []*types.Log) event.Subscription {
+func (b *LEthAPIBackend) SubscribeLogsEvent(ch chan<- []*types.Log) event.Subscription {
 	return b.leth.BlockChain().SubscribeLogsEvent(ch)
 }
 
-func (b *EthAPIBackend) SubscribePendingLogsEvent(ch chan<- []*types.Log) event.Subscription {
+func (b *LEthAPIBackend) SubscribePendingLogsEvent(ch chan<- []*types.Log) event.Subscription {
 	panic("not implement")
 }
 
-func (b *EthAPIBackend) SubscribeRemovedLogsEvent(ch chan<- core.RemovedLogsEvent) event.Subscription {
+func (b *LEthAPIBackend) SubscribeRemovedLogsEvent(ch chan<- core.RemovedLogsEvent) event.Subscription {
 	return b.leth.BlockChain().SubscribeRemovedLogsEvent(ch)
 }
 
-func (b *EthAPIBackend) SubscribeNewVoteEvent(chan<- core.NewVoteEvent) event.Subscription {
+func (b *LEthAPIBackend) SubscribeNewVoteEvent(chan<- core.NewVoteEvent) event.Subscription {
 	panic("not implement")
 }
 
-func (b *EthAPIBackend) SubscribeFinalizedHeaderEvent(ch chan<- core.FinalizedHeaderEvent) event.Subscription {
+func (b *LEthAPIBackend) SubscribeFinalizedHeaderEvent(ch chan<- core.FinalizedHeaderEvent) event.Subscription {
 	panic("not implement")
 }
 
-func (b *EthAPIBackend) ChainConfig() *params.ChainConfig {
+func (b *LEthAPIBackend) ChainConfig() *params.ChainConfig {
 	return b.leth.BlockChain().Config()
 }
 
-func (b *EthAPIBackend) Engine() consensus.Engine {
+func (b *LEthAPIBackend) Engine() consensus.Engine {
 	panic("not implement")
 }
 
-func (b *EthAPIBackend) StateAtBlock(ctx context.Context, block *types.Block, reexec uint64, base *state.StateDB, checkLive, preferDisk bool) (*state.StateDB, error) {
+func (b *LEthAPIBackend) StateAtBlock(ctx context.Context, block *types.Block, reexec uint64, base *state.StateDB, checkLive, preferDisk bool) (*state.StateDB, error) {
 	panic("not implement")
 }
 
-func (b *EthAPIBackend) StateAtTransaction(ctx context.Context, block *types.Block, txIndex int, reexec uint64) (core.Message, vm.BlockContext, *state.StateDB, error) {
+func (b *LEthAPIBackend) StateAtTransaction(ctx context.Context, block *types.Block, txIndex int, reexec uint64) (core.Message, vm.BlockContext, *state.StateDB, error) {
 	panic("not implement")
 }
--- cmd/explorer/leth/sync.go
+++ cmd/explorer/leth/sync.go
@@ -16,8 +16,7 @@ import (
 )
 
 const (
-	ethEventNewHeads               = "newHeads"
-	ethEventNewPendingTransactions = "newPendingTransactions"
+	ethEventNewHeads = "newHeads"
 )
 
 type SyncProgress struct {
@@ -31,8 +30,6 @@ type SyncProgress struct {
 type chainSyncer struct {
 	connector  *RpcConnector
 	blockchain *core.BlockChain
-	txpool     *core.TxPool
-	downloader *Downloader
 	wg         sync.WaitGroup
 	quitCh     chan struct{}
 }
@@ -108,65 +105,23 @@ func (s *chainSyncer) processNewHeads(wg *sync.WaitGroup) {
 	for head := range headCh {
 		parentBlock := s.blockchain.GetBlockByHash(head.ParentHash)
 		if parentBlock != nil {
-			s.downloader.ImportBlock(head.Hash())
+			s.importBlock(head.Hash())
 			continue
-		}
-		if !s.downloader.Synchronising() {
-			go s.downloader.StartSync(head.Number.Uint64())
 		}
 	}
 }
 
-// processNewPendingTxs listen and import pending transactions from RPC node to local txpool
-func (s *chainSyncer) processNewPendingTxs(wg *sync.WaitGroup) {
-	defer wg.Done()
-	txHashCh := make(chan common.Hash)
-	listenPendingTxs := func(rpcClient *rpc.Client) error {
-		sub, err := rpcClient.EthSubscribe(context.Background(), txHashCh, ethEventNewPendingTransactions)
-		if err != nil {
-			return err
-		}
-		select {
-		case <-s.quitCh:
-			return nil
-		case err := <-sub.Err():
-			return err
-		}
+func (s *chainSyncer) importBlock(hash common.Hash) error {
+	ctx, cancel := context.WithTimeout(context.Background(), downloadTimeout)
+	defer cancel()
+	block, err := ethclient.NewClient(s.connector.Client()).BlockByHash(ctx, hash)
+	if err != nil {
+		return err
 	}
-
-	addTxPool := func(rpcClient *rpc.Client, txHash common.Hash) {
-		if !s.downloader.Synchronising() {
-			client := ethclient.NewClient(rpcClient)
-			tx, isPending, err := client.TransactionByHash(context.Background(), txHash)
-			if err != nil {
-				return
-			}
-			if isPending {
-				s.txpool.AddRemotes(types.Transactions{tx})
-			}
-		}
+	if _, err := s.blockchain.InsertChain(types.Blocks{block}); err != nil {
+		return err
 	}
-
-	var rpcClient *rpc.Client
-	go func() {
-		defer close(txHashCh)
-		for ; ; time.Sleep(500 * time.Millisecond) {
-			rpcClient, _ = s.connector.Connect()
-			err := listenPendingTxs(rpcClient)
-			if err == rpc.ErrNotificationsUnsupported || err == rpc.ErrSubscriptionNotFound {
-				log.Warn("Could not sync txpool. RPC node subscription failed.", "event", ethEventNewPendingTransactions, "error", err)
-				return
-			}
-			if err != nil {
-				log.Warn("Event subscription broken. Reconnect to rpc node...", "event", ethEventNewPendingTransactions, "error", err)
-			} else {
-				return
-			}
-		}
-	}()
-	for txHash := range txHashCh {
-		go addTxPool(rpcClient, txHash)
-	}
+	return nil
 }
 
 // startSync starts it own goroutines to process events from remote automatically restarts when rpc connection closed
@@ -174,7 +129,6 @@ func (s *chainSyncer) runSync() {
 	s.quitCh = make(chan struct{})
 	s.wg.Add(2)
 	go s.processNewHeads(&s.wg)
-	go s.processNewPendingTxs(&s.wg)
 	s.wg.Wait()
 }
 
@@ -183,12 +137,10 @@ func (s *chainSyncer) stop() {
 	s.wg.Wait()
 }
 
-func newChainSyncer(connector *RpcConnector, blockchain *core.BlockChain, txpool *core.TxPool) *chainSyncer {
+func newChainSyncer(connector *RpcConnector, blockchain *core.BlockChain) *chainSyncer {
 	return &chainSyncer{
 		connector:  connector,
 		blockchain: blockchain,
-		txpool:     txpool,
-		downloader: NewDownloader(connector, blockchain),
 		wg:         sync.WaitGroup{},
 	}
 }
