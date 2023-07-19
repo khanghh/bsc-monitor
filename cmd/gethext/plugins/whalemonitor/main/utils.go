e01d3c3cfeed46c24d768d8bb9a98744300a7723
Khang Hoang <hoanghongkhang@gmail.com>
2023-07-19 14:00:51 +0700

    refactor(whalemonitor): detect whale token transfer

--- cmd/gethext/plugins/whalemonitor/main/utils.go
+++ cmd/gethext/plugins/whalemonitor/main/utils.go
@@ -1,9 +1,36 @@
 package main
 
 import (
+	"errors"
 	"math"
 	"math/big"
+
+	"github.com/ethereum/go-ethereum/cmd/gethext/abiutils"
+	"github.com/ethereum/go-ethereum/common"
 )
+
+type ERC20TransferArgs struct {
+	From   common.Address
+	To     common.Address
+	Amount *big.Int
+}
+
+func ParseERC20TransferArgs(sender common.Address, erc20 *abiutils.Interface, input []byte) (*ERC20TransferArgs, error) {
+	methodSig, data := input[0:4], input[4:]
+	method, err := erc20.MethodById(methodSig)
+	if err != nil {
+		return nil, err
+	}
+	if method.RawName == "transfer" || method.RawName == "transferFrom" {
+		var args ERC20TransferArgs
+		args.From = sender
+		if err := erc20.UnpackInput(&args, method.RawName, data); err != nil {
+			return nil, err
+		}
+		return &args, nil
+	}
+	return nil, errors.New("not a token transfer")
+}
 
 func AmountFloat64(val *big.Int, decimals uint64) float64 {
 	expDec := new(big.Float).SetInt(new(big.Int).Exp(big.NewInt(10), big.NewInt(int64(decimals)), nil))
