ea73e0fe302a9f2f1ec58639d3c54e3ad71961eb
Khang Hoang <hoanghongkhang@gmail.com>
2023-07-27 14:36:19 +0700

    feat(leth): [WIP] implement LightChain with Off-chain Data Retrival

---
+++ cmd/explorer/leth/odr.go
@@ -0,0 +1,97 @@
+package leth
+
+import (
+	"time"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/ethdb"
+	"github.com/ethereum/go-ethereum/rpc"
+)
+
+var (
+	rpcTimeout = time.Duration(5 * time.Second)
+)
+
+// OdrBackend is Off-chain Data Retrieval backend for LightEthereum
+type OdrBackend interface {
+	// Database returns the database used by the backend.
+	Database() ethdb.Database
+
+	// GetBlockByNumber retrieves a block by its block number.
+	GetBlockByNumber(number uint64) (*types.Block, error)
+
+	// GetBlockByHash retrieves a block by its block hash.
+	GetBlockByHash(hash common.Hash) (*types.Block, error)
+
+	// GetChainSegment retrieves a chain segment with a specified number of blocks starting from 'headBlock',
+	// return the chain segment and highest block number of the remote chain
+	GetChainSegment(headBlock *types.Block, count uint64) (types.Blocks, uint64, error)
+
+	// GetCodeAt retrieves the code stored at 'addr'.
+	GetCodeAt(addr common.Address) ([]byte, error)
+
+	// GetTransactionByHash retrieves a transaction by its hash.
+	GetTransactionByHash(hash common.Hash) (*types.Transaction, common.Hash, uint64, uint64, error)
+
+	// GetTransactionReceipt retrieves the receipt of a transaction identified by its hash.
+	GetTransactionReceipt(hash common.Hash) (*types.Receipt, error)
+
+	// GetBlockReceipts retrieves all receipts for transactions included in the given block.
+	GetBlockReceipts(block *types.Block) (types.Receipts, error)
+
+	// Close closes the backend connection and releases any associated resources.
+	Close()
+}
+
+type RpcOdr struct {
+	db     ethdb.Database
+	client *rpc.Client
+}
+
+func (r *RpcOdr) Database() ethdb.Database {
+	return r.db
+}
+
+func (r *RpcOdr) GetBlockByNumber(number uint64) (*types.Block, error) {
+	return nil, nil
+}
+
+func (r *RpcOdr) GetBlockByHash(hash common.Hash) (*types.Block, error) {
+	return nil, nil
+}
+
+func (r *RpcOdr) GetChainSegment(headBlock *types.Block, maxBlock uint64) (types.Blocks, uint64, error) {
+	return nil, 0, nil
+}
+
+func (r *RpcOdr) GetCodeAt(addr common.Address) ([]byte, error) {
+	return nil, nil
+}
+
+func (r *RpcOdr) GetTransactionByHash(hash common.Hash) (*types.Transaction, common.Hash, uint64, uint64, error) {
+	return nil, common.Hash{}, 0, 0, nil
+}
+
+func (r *RpcOdr) GetTransactionReceipt(hash common.Hash) (*types.Receipt, error) {
+	return nil, nil
+}
+
+func (r *RpcOdr) GetBlockReceipts(block *types.Block) (types.Receipts, error) {
+	return nil, nil
+}
+
+func (r *RpcOdr) Close() {
+	r.client.Close()
+}
+
+func NewRpcOdr(db ethdb.Database, rpcUrl string) (*RpcOdr, error) {
+	client, err := rpc.DialHTTP(rpcUrl)
+	if err != nil {
+		return nil, err
+	}
+	return &RpcOdr{
+		db:     db,
+		client: client,
+	}, nil
+}
